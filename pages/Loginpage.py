2b9facab081bb9b4c798d173c487cb573f360224
varunmadineni <varunmadineni08@gmail.com>
2025-05-15 15:44:45 +0530

    Login page

--- pages/Loginpage.py
+++ pages/Loginpage.py
@@ -1,3 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+class LoginPage:
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    username_id="user-name"
+    password_id="password"
+    login_button_id="login-button"
+
+
+    def enter_username(self,username):
+        self.driver.find_element(By.ID,self.username_id).clear()
+        self.driver.find_element(By.ID,self.username_id).send_keys(username)
+
+    def enter_password(self,password):
+        self.driver.find_element(By.ID,self.password_id).clear()
+        self.driver.find_element(By.ID,self.password_id).send_keys(password)
+
+    def click_login_button(self):
+        self.driver.find_element(By.ID,self.login_button_id).click()
+
+
